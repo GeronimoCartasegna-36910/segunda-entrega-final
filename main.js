9b4595cb1ae8023ed67c3f67f01b8995de85ec64
GeronimoCartasegna-36910 <gero.cartasegna17@gmail.com>
2022-07-31 20:54:14 -0300

    errores solucionados, proyecto finalizado

--- main.js
+++ main.js
@@ -32,7 +32,7 @@ const divListado = document.getElementById("divListado")
 
 //AGREGO EL EVENTO AL FORM
 idForm.addEventListener("submit", (e) =>{
-    e.preventDefault
+    e.preventDefault()
     let datosForm = new FormData(e.target)
     let auto = new Auto(datosForm.get("marca"), datosForm.get("modelo"), datosForm.get("precio"), datosForm.get("stock"))
     autos.push(auto)
@@ -40,8 +40,6 @@ idForm.addEventListener("submit", (e) =>{
 
     idForm.reset()
 })
-
-
 
 
 //AGREGO LA CARD DONDE SE VA A VER EL LISTADO DE AUTOS MEDIANTE EL INNERHTML
@@ -52,28 +50,27 @@ btnListado.addEventListener("click", () => {
     
     arrayStorge.forEach((auto, indice) => {
         divListado.innerHTML += `
-        <div class="card text-white bg-dark mb-3" id="auto${indice} " style="max-width: 20rem; margin:5px;">
+        <div class="card text-white bg-dark mb-3" id="auto${indice}" style="max-width: 20rem; margin:5px;">
             <div class="card-header"><h2>${auto.modelo}</h2></div>
                 <div class="card-body">
                     <p class="card-text">Marca: ${auto.marca}</p>
                     <p class="card-text">Precio: ${auto.precio}</p>
                     <p class="card-text">Stock: ${auto.stock}</p>
-                    <button class="btn bnt-danger">Eliminar Auto</button>
+                    <button class="btn btn-danger">Eliminar Auto</button>
                 </div>
         </div>
         `
     });
 
  //BUSCO EL BOTON ELIMINAR MEDIANTE LA PROPIEDAD "lastElementChil" Y LE AGREGO UNA FUNCION LA CUAL VA A ELIMINAR EL OBJETO DEL DOM, DEL ARRAY Y DEL LOCAL STORAGE
-    arrayStorge.forEach((autos, indice) =>{
-        let btnEliminar = document.getElementById(`auto${indice} `).lastElementChild.lastElementChild
-        btnEliminar.addEventListener("click", ()=>{
-            document.getElementById(`tarea${indice}`).remove()
+    
+    arrayStorge.forEach((auto, indice) =>{
+        let btnEliminar = (document.getElementById(`auto${indice}`).lastElementChild.lastElementChild)
+
+        btnEliminar.addEventListener("click", () => {
+            document.getElementById(`auto${indice}`).remove()
             autos.splice(indice,1)
             localStorage.setItem("autos", JSON.stringify(autos))
         })
-
     })
-
-
 })
